b270555f043ecbd79fb6ffdb9954ffede1394276
Bikash Upadhaya <71246916+bikashupadhay16@users.noreply.github.com>
2023-08-28 22:14:29 +0530

    initial init

---
+++ JS/store.js
@@ -0,0 +1,146 @@
+const myQuestions = [
+    {
+        question: "What is a string in JavaScript？",
+        answers: {
+            a: "JavaScript string that you can tie your notes to",
+            b: "JavaScript string that you can pluck and create musical sound",
+            c: "JavaScript strings are used for storing and manipulating text",
+            d: "All of the above"
+        },
+        correctAnswer: "c",
+        correctAnswerResult: "JavaScript strings are used for storing and manipulating text"
+    },
+    {
+        question: "How do you find the number with the highest value of x and y?",
+        answers: {
+            a:  "top(x,y)",
+            b:  "Math.celi(x,y)" ,
+            c:  "Math.max(x,y)",
+            d:  "celi(x,y)"
+        },
+        correctAnswer: "c",
+        correctAnswerResult: "Math.max(x,y)"
+    },
+    {
+        question: "How do you write 'Hello World' in an alert box?",
+        answers: {
+            a: "msg('Hello World')",
+            b: "alertBox('Hello world')",
+            c: "alert('Hello World')",
+            d: "All of the above"
+        },
+        correctAnswer: "c",
+        correctAnswerResult: "alert('Hello World')"
+    },
+    {
+        question: "Where is the correct place to insert a JavaScript? ",
+        answers: {
+            a: "The body section",
+            b: "The head section",
+            c: "None of the above",
+            d: "All of the above"
+        },
+        correctAnswer: "a",
+        correctAnswerResult: "The body section"
+    },
+    {
+        question: "How do you create a function in JavaScript?",
+        answers: {
+            a: "Function:myfunction",
+            b: "function myfunction()",
+            c: "Both a and b ",
+            d: "None of the above"
+        },
+        correctAnswer: "b",
+        correctAnswerResult: "function myfunction()"
+    },
+    {
+        question: "How do you call a function named 'myFunction'?",
+        answers: {
+            a: "call function myfunction",
+            b: "call myfunction",
+            c: "myFunction()",
+            d: "a and b"
+        },
+        correctAnswer: "c",
+        correctAnswerResult: "myFunction()"
+    },
+    {
+        question: "How to write an IF statement in JavaScript?",
+        answers: {
+            a: "if i == 5 then",
+            b: "if i = 5",
+            c: "if (i === 5)",
+            d: "None of the above"
+        },
+        correctAnswer: "c",
+        correctAnswerResult: "if (i === 5)"
+    },
+    {
+        question: "How to write an IF statement for executing some code if  i  is NOT equal to 5?",
+        answers: {
+            a: "if i =! 5 then ",
+            b: "if (i<>5)",
+            c: "if (i != 5)",
+            d: "All of the above"
+        },
+        correctAnswer: "c",
+        correctAnswerResult: "if (i != 5)"
+    },
+    {
+        question: " What is the correct way to write a JavaScript array?",
+        answers: {
+            a: "var color = red, blue, green ",
+            b: "var color = [ red, blue, green] ",
+            c: "let color = ' red, blue, green ' ",
+            d: "a and c"
+        },
+        correctAnswer: "b",
+        correctAnswerResult: "var color = [ red, blue, green] "
+    },
+    {
+        question: "What is a function?",
+        answers: {
+            a: "A block of code designed to perform a specific task or calculate a value",
+            b: "A semantic tool that is invoked when a particular action is desired",
+            c: "An action that may be performed on an object",
+            d: "A container for a data value"
+        },
+        correctAnswer: "a",
+        correctAnswerResult: "A block of code designed to perform a specific task or calculate a value"
+    },
+    {
+        question: "What is the correct JavaScript syntax for opening a new window called w2",
+        answers: {
+            a: "w2 = window.new('http://www.thinkful.com')",
+            b: "w2 = window.create('http://www.thinkful.com')",
+            c: "a and b",
+            d: "w2 = window.open('http://www.thinkful.com')"
+        },
+        correctAnswer: "d",
+        correctAnswerResult: "w2 = window.open('http://www.thinkful.com')"
+    },
+    {
+        question: "How does a FOR loop start?",
+        answers: {
+            a: "for( i=0 ; i<=5; i++)",
+            b: "for (i=1)",
+            c: "for(i =0, i< 5, i++)",
+            d: "None of above"
+        },
+        correctAnswer: "a",
+        correctAnswerResult: "for( i=0 ; i<=5; i++)"
+    },
+    {
+        question: "How can you detect the client's browser name?",
+        answers: {
+            a: "client.navName",
+            b: "browser.name",
+            c: "navigator.appName",
+            d: "All of above "
+        },
+        correctAnswer: "b",
+        correctAnswerResult: "browser.names",
+    }
+
+];
